deb404a5aa628885d13c59ddd155d988b127cbb5
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2018-10-25 11:38:07 +0530

    added  two functions for counter.
    counter from zero and counter from N'th number are working

--- src/lib.js
+++ src/lib.js
@@ -1,11 +1,34 @@
 const makeConstant = undefined;
-const makeCounterFromN = undefined;
-const makeCounterFromZero = undefined;
 const makeDeltaTracker = undefined;
-const makeFiboGenerator = undefined;
 const makeCycler = undefined;
 const curry = undefined;
 const compose = undefined;
+
+const makeFiboGenerator = function (num1,num2){
+  let lastSecondTerm = num1;
+  let previousTerm = num2;
+  let currentTerm = 0;
+  const fibo = function(){
+    currentTerm = previousTerm+lastSecondTerm;
+    lastSecondTerm = previousTerm;
+    previousTerm = currentTerm;
+    return(currentTerm);
+  }
+  return fibo;
+}
+
+const makeCounterFromZero=function(){
+  let counter = 0;
+  return function(){
+    return counter ++;
+  }
+}
+const makeCounterFromN=function(counterStartNumber){
+  let counter = counterStartNumber;
+  return function(){
+    return counter ++;
+  }
+}
 
 exports.makeConstant=makeConstant;
 exports.makeCounterFromZero=makeCounterFromZero;
